4815c37f1a2017ec7a52838ebda09de8a50e955c
nathansilva <rm88517@fiap.com.br>
2022-01-17 20:14:36 -0300

    Script ativar links menu

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//Ativar links do menu, é bom ter cada script separado
+
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
